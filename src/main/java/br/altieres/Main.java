9a5ad969656ba39cd7cbbbb7573aaa59975723f4
Altieres <altieres.silva@hotmail.com>
2024-11-14 23:45:54 -0300

    Correção da sintaxe

--- src/main/java/br/altieres/Main.java
+++ src/main/java/br/altieres/Main.java
@@ -1,7 +1,20 @@
 package br.altieres;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Carro> listaCarros = new ArrayList<>();
+
+        listaCarros.add(new Carro("Ford"));
+        listaCarros.add(new Carro("Chevrolet"));
+        listaCarros.add(new Carro("Fiat"));
+
+        System.out.println(listaCarros.contains(new Carro("Ford")));
+        System.out.println(new Carro("Ford").hashCode());
+        System.out.println(new Carro("Ford").hashCode());
+
     }
 }
